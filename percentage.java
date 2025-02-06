b40370a1d6a95322f5642c690ce8e800f0fcaae1
Akansha More <163007587+AkanshaBhoi@users.noreply.github.com>
2025-02-06 22:36:41 +0530

    percentage or grades of structure

---
+++ percentage.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class percentage 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the percantage: ");
+		float marks =sc.nextFloat();
+		float per=(marks*100)/600;
+		String op=(per>=75)?("Grade A with"+per+"%"):(per>=60)?("Grade B with"+per+"%"):(per>=35)?("Grade C with"+per+"%"):("Failed");
+		System.out.println(op);
+	}
+}
