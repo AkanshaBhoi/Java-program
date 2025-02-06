91b8d14f3f3bafc73129aa8daddffae3493c9dac
Akansha More <163007587+AkanshaBhoi@users.noreply.github.com>
2025-02-06 22:52:09 +0530

    Election mechine

---
+++ EV.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+class EV 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int bjp=0,cng=0,ss=0,aap=0,mns=0,no=0;
+		System.out.println();
+		System.out.println("    WELCOME     ");
+		System.out.println();
+		System.out.println("Enter population :");
+		int population=sc.nextInt();
+		for(int i=1;i<=population;i++);
+		{
+			System.out.println();
+			System.out.println("*** List of Parties ***");
+			System.out.println("1. BJP");
+			System.out.println("2. CONGRESS");
+			System.out.println("3. SHIV SENA");
+			System.out.println("4. AAP");
+			System.out.println("5. MNS");
+			System.out.println("6.NOTA");
+			System.out.println("");
+			System.out.println("Enter the option : ");
+			int opt=sc.nextInt();
+			if(opt>=1 && opt<=6){
+			if(opt==1)
+			{
+				bjp++;
+				System.out.println("Ache din ayenge");
+			}
+			if(opt==2)
+			{
+				cng++;
+				System.out.println("Bharat todo");
+			}
+			if(opt==3)
+			{
+				ss++;
+				System.out.println("Hum hai Asli Sena");
+			}
+			if(opt==4)
+			{
+				aap++;
+				System.out.println("Muze Azad karo");
+			}
+			if(opt==5)
+			{
+				mns++;
+				System.out.println("Jai maharashta");
+			}
+			if(opt==6)
+			{
+				no++;
+				System.out.println("You are an educated Person");
+			}
+			
+		}
+		
+	}
+		
+	}
+}
