5253df0ef1ea6971d0508d4b944f4df940953802
Akansha More <163007587+AkanshaBhoi@users.noreply.github.com>
2025-02-06 22:24:23 +0530

    letter is uppercase or not

---
+++ upper.java
@@ -0,0 +1,14 @@
+ class upper 
+{
+	public static void main(String[] args) 
+	{
+		
+		char ch='A';
+
+		String lar=((ch>='A'&& ch<='Z')?("This is uppercase letter"):("This is not a uppercase letter"));
+
+		System.out.println(lar);
+
+
+	}
+}
